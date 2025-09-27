09008f7ae07922724025482447dbde2af29f9065
unknown <vitaekasaraswati16@gmail.com>
2025-09-27 23:22:06 +0700

    feat(main): add entry point

--- week 3/jobsheet_3/bin/jobsheet_3.dart
+++ week 3/jobsheet_3/bin/jobsheet_3.dart
@@ -1,5 +1,29 @@
-import 'package:jobsheet_3/jobsheet_3.dart' as jobsheet_3;
+import 'dart:io';
+import 'package:jobsheet_3/faktorial.dart';
+import 'package:jobsheet_3/prima.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${jobsheet_3.calculate()}!');
+void main() {
+  while (true) {
+    print('\n=== Menu Program ===');
+    print('1. Hitung Faktorial');
+    print('2. Cek Bilangan Prima');
+    print('3. Keluar');
+    stdout.write('Pilih menu (1-3): ');
+    var choice = int.parse(stdin.readLineSync()!);
+
+    if (choice == 1) {
+      stdout.write('Masukkan angka: ');
+      int n = int.parse(stdin.readLineSync()!);
+      print('Faktorial dari $n = ${hitungFaktorial(n)}');
+    } else if (choice == 2) {
+      stdout.write('Masukkan angka: ');
+      int n = int.parse(stdin.readLineSync()!);
+      print('$n adalah bilangan ${isPrime(n) ? "Prima" : "Bukan Prima"}');
+    } else if (choice == 3) {
+      print('Program selesai. Bye!');
+      break;
+    } else {
+      print('Pilihan tidak valid.');
+    }
+  }
 }
