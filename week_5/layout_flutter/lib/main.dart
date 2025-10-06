2fc5e564e6ec711f8cced3c4247a194351a20c07
unknown <vitaekasaraswati16@gmail.com>
2025-10-07 00:57:09 +0700

    feat(main): praktikum 1.4 - implement title row

--- week_5/layout_flutter/lib/main.dart
+++ week_5/layout_flutter/lib/main.dart
@@ -7,14 +7,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Langkah 4: Implementasi title row
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /* soal 1 */
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /* soal 2 */
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /* soal 3 */
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter layout: Vita Eka Saraswati 2341760082',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        // Ganti body menjadi titleSection
+        body: Column(
+          children: [
+            titleSection,
+          ],
         ),
       ),
     );
