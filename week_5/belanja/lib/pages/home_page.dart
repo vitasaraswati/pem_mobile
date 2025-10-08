0be8c5f443690a88f641ac04f6922501c013c65f
unknown <vitaekasaraswati16@gmail.com>
2025-10-09 00:02:15 +0700

    feat(item): tugas 2.3  add product attributes & change layout to GridView

--- week_5/belanja/lib/pages/home_page.dart
+++ week_5/belanja/lib/pages/home_page.dart
@@ -1,20 +1,37 @@
-import 'package:belanja/models/item.dart';
 import 'package:flutter/material.dart';
+import 'package:belanja/models/item.dart';
 
 class HomePage extends StatelessWidget {
   final List<Item> items = [
-    Item(name: 'Sugar', price: 5000),
-    Item(name: 'Salt', price: 2000),
+    Item(
+      name: 'Sugar',
+      price: 5000,
+      image: 'images/sugar.jpeg',
+      stock: 10,
+      rating: 4.5,
+    ),
+    Item(
+      name: 'Salt',
+      price: 2000,
+      image: 'images/salt.jpeg',
+      stock: 8,
+      rating: 4.2,
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Belanja')),
-      body: Container(
-        margin: EdgeInsets.all(8),
-        child: ListView.builder(
-          padding: EdgeInsets.all(8),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // 2 kolom seperti marketplace
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+            childAspectRatio: 0.8,
+          ),
           itemCount: items.length,
           itemBuilder: (context, index) {
             final item = items[index];
@@ -23,19 +40,43 @@ class HomePage extends StatelessWidget {
                 Navigator.pushNamed(context, '/item', arguments: item);
               },
               child: Card(
-                child: Container(
-                  margin: EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text(item.name)),
-                      Expanded(
-                        child: Text(
-                          item.price.toString(),
-                          textAlign: TextAlign.end,
-                        ),
+                elevation: 4,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Hero(
+                        tag: item.name,
+                        child: Image.asset(item.image, fit: BoxFit.cover),
                       ),
-                    ],
-                  ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            item.name,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            'Rp${item.price}',
+                            style: TextStyle(color: Colors.grey[700]),
+                          ),
+                          Text('Stok: ${item.stock}'),
+                          Row(
+                            children: [
+                              Icon(Icons.star, color: Colors.amber, size: 16),
+                              Text('${item.rating}'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
             );
--- week_5/belanja/lib/pages/item_page.dart
+++ week_5/belanja/lib/pages/item_page.dart
@@ -9,11 +9,13 @@ class ItemPage extends StatelessWidget {
     final Item? itemArgs = ModalRoute.of(context)?.settings?.arguments as Item?;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Belanja'),
-      ),
+      appBar: AppBar(title: Text('Belanja')),
       body: Center(
-        child: Text(itemArgs?.name != null ? '${itemArgs!.name} with ${itemArgs!.price}' : 'No item selected'),
+        child: Text(
+          itemArgs?.name != null
+              ? '${itemArgs!.name} with ${itemArgs!.price}'
+              : 'No item selected',
+        ),
       ),
     );
   }
